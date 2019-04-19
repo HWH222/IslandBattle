71275b361bca773b9b9a5ba1b1db964ebc32bfe1
HWH222 <32691747+HWH222@users.noreply.github.com>
2019-04-19 16:32:44 +0800

    4.19 cpp

---
+++ Source/IslandBattle/Private/IBIsland.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IBIsland.h"
+
+// Sets default values
+AIBIsland::AIBIsland()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AIBIsland::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AIBIsland::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/IslandBattle/Public/IBIsland.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "IBIsland.generated.h"
+
+UCLASS()
+class ISLANDBATTLE_API AIBIsland : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AIBIsland();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Resource")
+	TSubclassOf<AActor>  GoldResource;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Resource")
+	TSubclassOf<AActor>  TreeResource;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
