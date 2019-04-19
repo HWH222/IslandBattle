cd501c0020454343cb3f8c2519eedc232c63c37c
HWH222 <490506511@qq.com>
2019-04-19 12:04:16 +0800

    Initial C++ commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# IslandBattle
+
+Developed with Unreal Engine 4
+
+IT MUST BE FUN IN FUTURE
---
+++ Source/IslandBattle/IslandBattle.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IslandBattle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IslandBattle, "IslandBattle" );
