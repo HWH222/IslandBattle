e89d3d9eb7cdaa214f3fce6926118b721e264178
bloodyjoker <767373506@qq.com>
2019-05-07 09:41:11 +0800

    RECOMMIT movementcomponent

---
+++ Source/IslandBattle/Public/INShipComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "INShipComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISLANDBATTLE_API UINShipComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+};
